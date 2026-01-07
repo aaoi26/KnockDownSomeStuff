ce764668d21022d10924ad1030aaf9e51bd851d2
aaoi26 <miyaaoi0622@icloud.com>
2026-01-08 01:34:33 +0900

    l

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -2,16 +2,17 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour
 {
-    public GameObject ballPrefab;
+    public Rigidbody ballPrefab;
+    public float throwPower = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        this.ballPrefab = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ballPrefab.AddForce((transform.forward+transform.up) * throwPower, ForceMode.Impulse);
     }
 }
